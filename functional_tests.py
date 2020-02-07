b88b332327276924c1bcca90662b94fa0f63f637
kalenshi <kalenshi@gmail.com>
2020-02-07 14:32:08 -0400

    Improved functional tests  Changes to be committed: 	modified:   functional_tests.py 	deleted:    geckodriver.log

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,37 @@ __author__ = "Kalenshi Katebe"
 from selenium import webdriver
 import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://127.0.0.1:8000")
 
-assert 'Django' in browser.title
+#############
+# USER STORY #
+#############
+# User has heard of this new app called todo list
+# user goes to checkout the home page
+# user notices the page title and header mention to-do list
+# user is invited right away to add a to-do list item
+# user enters, 'finish chapter 3 of algorithms' (see user is a bookworm)
+# when user hits enter, user is faced with the same input box and one to-do list item on top of it
+# 1. finish chapter 3 of algorithm
+# user enters another to-do list item (complete week 3 of machine learning)
+# user hits enter and sees the two to-do list items and an input area
+
+
+# user is satisfied and quits
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://127.0.0.1:8000")
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
